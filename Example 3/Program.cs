22f5b5db6b67b828abc41b4ef5e36c7d85b5b932
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-04 13:02:04 +0530

    Add files via upload

---
+++ Example 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchronous_Programming_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1();
+            Method2();
+            Console.ReadKey();
+        }
+
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    // Do something
+                    Thread.Sleep(3000);
+                }
+            });
+        }
+
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(" Method 2");
+                // Do something
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
