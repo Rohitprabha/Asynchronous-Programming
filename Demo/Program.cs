7fd549f64b1076fb91f6d38b634f49ac9d297770
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-04 12:56:17 +0530

    Add files via upload

---
+++ Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchronous_Programming_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1();
+            Console.WriteLine("Main thread");
+            Console.ReadKey();
+        }
+
+        public static async void Method1()
+        {
+            await Task.Run(() => LongTask());
+            Console.WriteLine("New Thread");
+        }
+        public static void LongTask()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
